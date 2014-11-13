a5040a331fbee8586de8ff562eedff02a689aef6
Piotrek <Piotrek@PiotrekLap>
2014-11-13 20:26:22 +0100

    Początek

--- src/map/Map.java
+++ src/map/Map.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mainSystem;
+package map;
 
 /**
  *
  * @author Piotrek
  */
-public class Simple {
+public class Map {
     
 }
