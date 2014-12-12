6429275dc2b5ed8e4a0475967e62b368c13a5639
Piotrek <Piotrek@PiotrekLap>
2014-12-12 22:43:31 +0100

    dodany planer

---
+++ src/logic/Planner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author Piotrek
+ */
+public class Planner {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    
+    
+}
